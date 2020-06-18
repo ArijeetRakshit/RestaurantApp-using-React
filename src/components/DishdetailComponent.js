8dcd156f24278596f056eb8255c6c9281bc5bf75
Arijeet Rakshit <49709000+ArijeetRakshit@users.noreply.github.com>
2020-06-18 16:03:05 +0530

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,12 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
-    constructor(props) {
-        super(props);
-    }
     
-    renderDish(dish) {
+    function RenderDish({dish}) {
         return(
             <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
@@ -18,7 +14,7 @@ class DishDetail extends Component {
         );
     }
     
-    renderComments(comments) {
+    function RenderComments({comments}) {
         let list_cmt=comments.map((cmt)=>{
             return(
                 <li key={cmt.id}>
@@ -42,16 +38,16 @@ class DishDetail extends Component {
         return(<div></div>);
     }
 
-    render(){
-        if (this.props.dish) {
+    const DishDetail = (props) => {
+        if (props.dish) {
             return (
                 <div className="container">
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
+                            <RenderDish dish={props.dish}/>
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
+                            <RenderComments comments={props.dish.comments}/>
                         </div>
                     </div>
                 </div>
@@ -59,5 +55,5 @@ class DishDetail extends Component {
         }
         return(<div></div>)
     }
-}
+
 export default DishDetail;
