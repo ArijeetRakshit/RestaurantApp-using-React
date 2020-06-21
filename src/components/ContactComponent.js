36306148539f0d1e52dc41894b07a47bb285851b
Arijeet Rakshit <49709000+ArijeetRakshit@users.noreply.github.com>
2020-06-21 22:49:14 +0530

    Uncontrolled Form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -132,21 +132,21 @@ class Contact extends Component {
                             <FormGroup row>
                                 <Label htmlFor="lastname" md={2}>Last Name</Label>
                                 <Col md={10}>
-                                    <Input type="text" id="lastname" name="lastname" placeholder="Last Name" value={this.state.lastname} valid={errors.lastname===''} invalid={errors.lastname!==''}onChange={this.handleInputChange} onBlur={this.handleBlur('lastname')} />
+                                    <Input type="text" id="lastname" name="lastname" placeholder="Last Name" value={this.state.lastname} valid={errors.lastname===''} invalid={errors.lastname!==''} onChange={this.handleInputChange} onBlur={this.handleBlur('lastname')} />
                                     <FormFeedback>{errors.lastname}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label htmlFor="telnum" md={2}>Contact Tel.</Label>
                                 <Col md={10}>
-                                    <Input type="tel" id="telnum" name="telnum" placeholder="Tel. Number" value={this.state.telnum} valid={errors.telnum===''} invalid={errors.telnum!==''}onChange={this.handleInputChange} onBlur={this.handleBlur('telnum')} />
+                                    <Input type="tel" id="telnum" name="telnum" placeholder="Tel. Number" value={this.state.telnum} valid={errors.telnum===''} invalid={errors.telnum!==''} onChange={this.handleInputChange} onBlur={this.handleBlur('telnum')} />
                                     <FormFeedback>{errors.telnum}</FormFeedback>
                                 </Col>
                             </FormGroup>
                             <FormGroup row>
                                 <Label htmlFor="email" md={2}>Email</Label>
                                 <Col md={10}>
-                                    <Input type="email" id="email" name="email" placeholder="Email" value={this.state.email} valid={errors.email===''} invalid={errors.email!==''}onChange={this.handleInputChange} onBlur={this.handleBlur('email')}/>
+                                    <Input type="email" id="email" name="email" placeholder="Email" value={this.state.email} valid={errors.email===''} invalid={errors.email!==''} onChange={this.handleInputChange} onBlur={this.handleBlur('email')}/>
                                     <FormFeedback>{errors.email}</FormFeedback>
                                 </Col>
                             </FormGroup>
